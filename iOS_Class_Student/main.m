58a310179b08297e009fba19e08927ac259ea5b7
4nn4bel <annabeltengingsing@gmail.com>
2015-02-22 12:36:48 +0800

    first commit by Annabel

---
+++ iOS_Class_Student/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS_Class_Student
+//
+//  Created by 4nn4bel on 22/2/15.
+//  Copyright (c) 2015 4nn4bel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Annabel!");
+    }
+    return 0;
+}
